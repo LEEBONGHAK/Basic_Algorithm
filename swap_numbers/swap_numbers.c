36e23b41637547416d7a207cbc568773b775464f
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-08-14 20:48:59 +0900

    Swap Numbers

---
+++ swap_numbers/swap_numbers.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void swap(int* n, int* m) {
+	int temp = *n;
+	*n = *m;
+	*m = temp;
+}
+
+int main() {
+
+	int a = 1, b = 2;
+
+	printf("before swap, a and b are %d %d\n\n", a, b);
+
+	swap(&a, &b);
+	printf("After swap, a and b are %d %d\n", a, b);
+
+}
