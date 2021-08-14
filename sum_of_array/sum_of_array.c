d1b3f16bcb09621a83223435e3771745198b1a9d
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-08-14 21:03:10 +0900

    Sum of Array

---
+++ sum_of_array/sum_of_array.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int sum_of_array(int arr[], int size) {
+
+	int sum = 0;
+
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+
+	return sum;
+}
+
+int main() {
+
+	int arr[5] = { 40,13,89,52,7 };
+
+	printf("array : ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	printf("Sum of array is %d\n", sum_of_array(arr, 5));
+
+	return 0;
+}
