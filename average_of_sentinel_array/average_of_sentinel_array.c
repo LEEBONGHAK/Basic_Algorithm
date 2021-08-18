4930e2cbe2a329c29b7897145db77a22866363c5
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-08-18 20:56:15 +0900

    Average of sentinel array

---
+++ average_of_sentinel_array/average_of_sentinel_array.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+
+	int sentinel_array[6] = { 40,13,89,52,-1 };
+	int count = 0, sum = 0;
+
+	while (sentinel_array[count] != -1) {
+		sum += sentinel_array[count];
+		count++;
+	}
+
+	printf("Average of sentinel array : %f\n", (float)sum / count);
+
+	return 0;
+}
