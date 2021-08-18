5e5b02c73084bad9315772aad821156977a6c811
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-08-18 20:56:00 +0900

    Count sentinel array

---
+++ count_element_in_array_using_sentinel/count_element_in_array_using_sentinel.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+
+	int sentinel_array[6] = { 40,13,89,52,7,-1 };
+	int count = 0;
+
+	while (sentinel_array[count] != -1) {
+		count++;
+	}
+
+	printf("Number of sentinel array : %d\n", count);
+
+	return 0;
+}
